a6df5f23535e1a872476f9732fa296fc65b89af5
Ngan Kim Khong <43501354+nk2303@users.noreply.github.com>
2020-04-12 19:28:58 -0700

    Revert "Revert "Nk2303/add controller methods and seed db"" (#6)

--- app/controllers/api/v1/chores_controller.rb
+++ app/controllers/api/v1/chores_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::ChoresController < ApplicationController
+
+
+    def show
+        chore = Chore.find(params[:id])
+        render json: chore
+    end
+
+
+    def create
+        chore = Chore.create(chore_params)
+        render json: chore
+    end
+
+    def update
+        chore = Chore.find(params[:id])
+        if chore.valid?
+            chore.update(chore_params)
+            render json: chore
+        else
+            render json: { error: 'failed to update chore' }, status: :not_acceptable
+        end
+    end
+
+    def destroy
+        chore = Chore.find(params[:id])
+        chore.destroy
+        render json: {message: "Chore successfully deleted"}
+    end
+
+    private
+
+    def chore_params
+        params.require(:chore).permit(:name, :description, :user_id, :location_id, :day, :icon, :completed)
+    end
+
 end
