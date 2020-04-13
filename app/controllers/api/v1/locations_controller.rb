87dda88e79e76061b18037f26881c76352a1396d
Ngan Kim Khong <43501354+nk2303@users.noreply.github.com>
2020-04-12 19:28:37 -0700

    Revert "Revert "Nk2303/add controller methods and seed db""

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::LocationsController < ApplicationController
+
+
+    def show
+        location = Location.find(params[:id])
+        render json: location
+    end
+
+
+    def create
+        location = Location.create(location_params)
+        render json: location 
+    end
+
+    def destroy
+        location = Location.find(params[:id])
+        location.destroy
+        render json: {message: "House successfully deleted"} 
+    end
+
+    private
+
+    def location_params
+        params.require(:location).permit(:name)
+    end
+
 end
