f6958f384082be87a408789b3b123499bee33063
github username <github email address>
2020-04-12 17:55:26 -0700

    adding controller methods and creating seed database

--- app/controllers/api/v1/chores_controller.rb
+++ app/controllers/api/v1/chores_controller.rb
@@ -1,2 +1,46 @@
 class Api::V1::ChoresController < ApplicationController
+
+    def index
+        chores = Chore.all
+        render json: chores
+    end
+
+    def show
+        chore = Chore.find(params[:id])
+        render json: chore
+    end
+
+    def new
+        chore = Chore.new
+        render json: chore 
+    end
+
+    def create
+        chore = Chore.create(chore_params)
+        render json: chore
+    end
+
+    def edit
+        chore = Chore.find(params[:id])
+        render json: chore
+    end
+
+    def update
+        chore = Chore.find(params[:id])
+        chore.update(chore_params)
+        render json: chore
+    end
+
+    def destroy
+        chore = Chore.find(params[:id])
+        chore.destroy
+        render json: {message: "Chore successfully deleted"}
+    end
+
+    private
+
+    def chore_params
+        params.require(:chore).permit(:name, :description, :user_id, :location_id, :day, :icon, :completed)
+    end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,54 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
+  
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def new
+        user = User.new
+    end
+
+    def edit
+        user = User.find(params[:id])
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.valid?
+            user.update(user_params)
+            render json: user
+        else
+            render json: { error: 'failed to update user' }, status: :not_acceptable
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render json: {message: "User successfully deleted"}
+    end
 
     def create 
-        @user = User.create(user_params)
-        if @user.valid?
-          @token = issue_token(@user)
-          render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        user = User.create(user_params)
+        if user.valid?
+          @token = issue_token(user)
+          render json: { user: UserSerializer.new(user), jwt: @token }, status: :created
         else
           render json: { error: 'failed to create user' }, status: :not_acceptable
         end
-      end
+    end
 
-      private
+    private
+
       def user_params
-        params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
+        params.require(:user).permit(:username, :password, :first_name, :last_name, :email, :location_id)
       end
     
 end
