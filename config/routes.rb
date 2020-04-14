18698eda4840a1993ecf0e844ddbcbfbfc093ef9
dianakw8591 <dianakw@gmail.com>
2020-04-13 22:03:20 -0700

    authorization modifications

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :chores
       resources :users
       post '/login', to: 'auth#create'
+      get '/current_user', to: 'auth#show'
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
