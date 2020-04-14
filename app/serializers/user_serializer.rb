e62f8ab461b66be703e5b7d56ac17870b2877ed6
dianakw8591 <59093722+dianakw8591@users.noreply.github.com>
2020-04-13 22:32:44 -0700

    additional authorization routes and functions (#8)
    * modification to errors that create user returns
    
    * authorization modifications

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name, :email
+  attributes :id, :username, :first_name, :last_name, :email, :location_id
 end
