111c16c299f6e1f9cf0aed9d8f186975ee3eed48
dianakw8591 <dianakw@gmail.com>
2020-04-13 17:27:12 -0700

    modification to errors that create user returns

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,7 +29,7 @@ class Api::V1::UsersController < ApplicationController
           @token = issue_token(user)
           render json: { user: UserSerializer.new(user), jwt: @token }, status: :created
         else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
+          render json: { error: user.errors.full_messages }, status: :not_acceptable
         end
     end
 
